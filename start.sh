524c5f476395eb4a8da1d56be70608521fdb0fee
Soumen Karmakar <soumen.karmakar@ericsson.com>
2020-12-08 12:27:13 +0000

    Changing the startup script

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-nohup java -jar -Djava.security.egd=file:/dev/./urandom target/RabbitMQ-Load-Tester-1.0.jar --spring.config.location=config.properties > out.log 2>&1 &
+nohup java -jar -Djava.security.egd=file:/dev/./urandom target/RabbitMQ-Load-Injector-1.0.jar --spring.config.location=config.properties > out.log 2>&1 &
 echo $! > app.pid
